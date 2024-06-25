312affa5588729052b967ba590a7e762f7c4a509
nombre <mateogazzola7@gmail.com>
2024-06-25 10:55:17 -0300

    avance con firebase y el shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { enviroment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentesComponent } from './componentes/componentes.component';
-import { CardComponent } from './components/card/card.component';
 import { ComponentsComponent } from './modules/inicio/components/components.component';
 import { PageComponent } from './modules/inicio/page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentesComponent,
-    CardComponent,
     ComponentsComponent,
     PageComponent
   ],
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
